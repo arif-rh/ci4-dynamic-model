4d2d5706225ba5b62bd9446901e5ddf940ba6607
Arif RH <arifrahmanhakim.net@gmail.com>
2020-04-20 06:56:24 +0700

    adding phpstan

---
+++ tests/Bootstrap.php
@@ -0,0 +1,10 @@
+<?php 
+define("HOMEPATH", __DIR__."/../");
+define("CONFIGPATH", __DIR__."/../vendor/codeigniter4/framework/app/Config/");
+define("PUBLICPATH", __DIR__."/../public/");
+define("TESTPATH", __DIR__."/../tests/");
+define("CI_DEBUG", "1");
+
+require_once __DIR__."/../vendor/codeigniter4/framework/system/Helpers/array_helper.php";
+require_once __DIR__."/../src/Helpers/array_helper.php";
+require_once  __DIR__."/../vendor/codeigniter4/framework/system/Test/bootstrap.php";
